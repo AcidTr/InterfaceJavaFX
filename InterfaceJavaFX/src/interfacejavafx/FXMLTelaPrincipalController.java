c5d420d949bf7f0457bed27252e2db96f758bff4
Victor Mourão <35748259+AcidTr@users.noreply.github.com>
2018-05-15 12:31:41 -0300

    Update FXMLTelaPrincipalController.java

--- InterfaceJavaFX/src/interfacejavafx/FXMLTelaPrincipalController.java
+++ InterfaceJavaFX/src/interfacejavafx/FXMLTelaPrincipalController.java
@@ -5,14 +5,12 @@
  */
 package interfacejavafx;
 
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -20,7 +18,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.MenuBar;
+
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -33,102 +32,112 @@ import javafx.stage.Stage;
 public class FXMLTelaPrincipalController implements Initializable {
 
     @FXML
-    private MenuBar menuBar;
-    
-    @FXML
-    private Pane pCadUser;
-    
-    @FXML
     private Pane panSobre;
-    
+
     @FXML
     private Stage stage;
-    
+
     @FXML
-    private Button cadUser;
-    
+    private Button btnCadUser;
+   
+    @FXML
+    private Button btnCadFunc;
+
     @FXML
     private Button btnSair;
-    
+
     @FXML
     private Button btnSobre;
-    
+
     @FXML
     private Hyperlink hlGit;
-    
+
     @FXML
-    private void hlGitOpen(){
+    private void hlGitOpen() {
         //ação para abrir link do GitHub no navegador.(pesquisar)
     }
-    
+
     @FXML
-    private void bVoltar(){
-        pCadUser.setVisible(false);
+    private void bVoltar() {
         panSobre.setVisible(false);
     }
-    
+
     @FXML
-    private void bSobre(){
+    private void bSobre() {
         panSobre.setVisible(true);
     }
-    
+
     @FXML
-    private void cadUsuario(){
-        if(panSobre.isVisible()){
+    private void cadUsuario() throws Exception{
+        if (panSobre.isVisible()) {
             panSobre.setVisible(false);
         }
-        pCadUser.setVisible(true);
-        
-       
+        Stage stage = new Stage();
+
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("FXMLTelaCadastroUsuarioSDI.fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLTelaCadastroUsuarioSDIController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        btnCadUser.getScene().getWindow().hide();
         
     }
-    
+   
+    @FXML
+    private void cadFunc() throws Exception{
+        if (panSobre.isVisible()) {
+            panSobre.setVisible(false);
+        }
+        Stage stage = new Stage();
+
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("FXMLTelaCadastroFunc.fxml")); //Carregar tela de cadastro de funcionários
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLTelaCadastroFuncController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        btnCadFunc.getScene().getWindow().hide();
+        
+    }
+
+    @FXML
+    private void bSair() throws Exception {
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("FXMLTelaLogin.fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLTelaLoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        Image app = new Image(getClass().getResourceAsStream("imagens/61205.png"));
+        stage.getIcons().add(app);
+        stage.show();
+        btnCadUser.getScene().getWindow().hide();
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         panSobre.setVisible(false);
-        pCadUser.setVisible(false);
-        cadUser.setGraphic(new ImageView(new javafx.scene.image.Image(getClass().getResourceAsStream("imagens/user_add.png"))));
-        cadUser.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                Stage stage = new Stage();
-                Parent root = null;
-                try{
-                    root = FXMLLoader.load(getClass().getResource("FXMLTelaCadastroUsuarioSDI.fxml"));
-                } catch (IOException ex) {
-                    Logger.getLogger(FXMLTelaCadastroUsuarioSDIController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.show();
-                cadUser.getScene().getWindow().hide();
-            }
-        });
-       
-        
+        btnCadUser.setGraphic(new ImageView(new javafx.scene.image.Image(getClass().getResourceAsStream("imagens/user_add.png"))));
         btnSobre.setGraphic(new ImageView(new javafx.scene.image.Image(getClass().getResourceAsStream("imagens/help.png"))));
-        
         btnSair.setGraphic(new ImageView(new javafx.scene.image.Image(getClass().getResourceAsStream("imagens/user_go.png"))));
-        btnSair.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                Stage stage = new Stage();
-                Parent root = null;
-                try{
-                    root = FXMLLoader.load(getClass().getResource("FXMLTelaLogin.fxml"));
-                } catch (IOException ex) {
-                    Logger.getLogger(FXMLTelaLoginController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.show();
-                cadUser.getScene().getWindow().hide();
-            }
-        });
+        btnCadFunc.setGraphic(new ImageView(new javafx.scene.image.Image(getClass().getResourceAsStream("imagens/group_add.png"))));
         
+
     }
-            
+
 }
