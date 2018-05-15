86e522c69d570c21f6d7dd3c027e28a67830ceb4
Victor Mourão <35748259+AcidTr@users.noreply.github.com>
2018-05-15 12:30:27 -0300

    Add files via upload
    Inclusão da tela de cadastro de funcionários

---
+++ InterfaceJavaFX/src/interfacejavafx/FXMLTelaCadastroFuncController.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacejavafx;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Victor
+ */
+public class FXMLTelaCadastroFuncController implements Initializable {
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private DatePicker dpNasc;
+
+    @FXML
+    private ToggleGroup sexo;
+    
+    @FXML
+    private void bVoltar() {
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("FXMLTelaPrincipal.fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLTelaPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        btnVoltar.getScene().getWindow().hide();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
